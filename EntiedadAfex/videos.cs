396d5c1226004af900e3986cfb56941841c1be15
BryanEncalada <93891074+BryanEncalada@users.noreply.github.com>
2022-11-17 15:43:00 -0500

    ok

---
+++ EntiedadAfex/videos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntiedadAfex
+{
+    public class videos
+    {
+        public int id { get; set; }
+        public string codigo { get; set; }
+        public string titulo { get; set; }
+        public string descripcion { get; set; }
+        public string imagen { get; set; }
+
+
+    }
+}
---
+++ conexionAfexmysql/ConeccionDA.cs
@@ -0,0 +1,114 @@
+﻿using EntiedadAfex;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conexionAfexmysql
+{
+    public class ConeccionDA
+    {
+
+        MySqlConnector.MySqlConnection conn;
+        MySqlConnector.MySqlCommand cmd;
+
+
+        static string cadenaconexion()
+        {
+
+            MySqlConnector.MySqlConnectionStringBuilder builder = new MySqlConnector.MySqlConnectionStringBuilder();
+
+            builder.Server = "localhost";
+            builder.Database = "afex";
+            builder.UserID = "root";
+            builder.Password = "Bryan0303@";
+            return builder.ToString();
+
+        }
+
+
+        public Boolean mantVideos(string tipo, videos video)
+        {
+
+            try
+            {
+                conn = new MySqlConnector.MySqlConnection(cadenaconexion());
+                conn.Open();
+                using (conn)
+                {
+                    using (cmd = new MySqlConnector.MySqlCommand("mantVideos", conn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@tipo", tipo);
+                        cmd.Parameters.AddWithValue("@pcodigo", video.codigo);
+                        cmd.Parameters.AddWithValue("@ptitulo", video.titulo);
+                        cmd.Parameters.AddWithValue("@pimagen", video.imagen);
+                        cmd.Parameters.AddWithValue("@pdescripcion", video.descripcion.Substring(0, 50));
+
+                        cmd.ExecuteNonQuery();
+
+
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                e = e;
+            }
+
+
+            return true;
+
+        }
+
+
+        public List<videos> ListarEnlace(string codigo)
+        {
+            List<videos> lVideo = new List<videos>();
+
+            try
+            {
+
+
+                conn = new MySqlConnector.MySqlConnection(cadenaconexion());
+                conn.Open();
+                using (conn)
+                {
+                    using (cmd = new MySqlConnector.MySqlCommand("ListarVideos", conn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@vCodigo", codigo);
+                        using (MySqlConnector.MySqlDataAdapter sda = new MySqlConnector.MySqlDataAdapter(cmd))
+                        {
+                            DataTable dt = new DataTable();
+                            sda.Fill(dt);
+
+                            foreach (DataRow variable in dt.Rows)
+                            {
+
+                                videos DEnlace = new videos();
+                                DEnlace.id = Convert.ToInt32(variable["id"]);
+                                DEnlace.codigo = variable["codigo"].ToString();
+                                DEnlace.titulo = variable["titulo"].ToString();
+                                DEnlace.descripcion = variable["descripcion"].ToString();
+                                DEnlace.imagen = variable["imagen"].ToString();
+
+                                lVideo.Add(DEnlace);
+
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+            return lVideo;
+        }
+
+    }
+}
